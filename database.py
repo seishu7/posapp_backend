6666a9832b323be0e0c963ff9401ff12f6c9a31e
Seishu Otori <seishu@SeishunoMacBook-Air.local>
2025-07-05 20:17:24 +0900

    feat: initial FastAPI project

---
+++ database.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+
+load_dotenv()  # .env ファイルを読み込む
+
+MYSQL_USER = os.getenv("DB_USER")
+MYSQL_PASSWORD = os.getenv("DB_PASSWORD")
+MYSQL_HOST = os.getenv("Db_HOST", "localhost")
+MYSQL_DB = os.getenv("")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True, pool_recycle=3600, future=True, connect_args={"ssl":{"ssl-ca": ssl_cert}} 
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
