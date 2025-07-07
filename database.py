d8fd13aec90817db0caf87edb0813f4332580be3
Seishu Otori <seishu@SeishunoMacBook-Air.local>
2025-07-07 17:22:15 +0900

    Fix: update for redeploy

--- database.py
+++ database.py
@@ -4,17 +4,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-
 load_dotenv()  # .env ファイルを読み込む
 
-MYSQL_USER = os.getenv("DB_USER")
-MYSQL_PASSWORD = os.getenv("DB_PASSWORD")
-MYSQL_HOST = os.getenv("Db_HOST", "localhost")
-MYSQL_DB = os.getenv("")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True, pool_recycle=3600, future=True, connect_args={"ssl":{"ssl-ca": ssl_cert}} 
+# 例: postgresql://postgres:password@your-host.supabase.co:6543/postgres?sslmode=require
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    future=True
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
